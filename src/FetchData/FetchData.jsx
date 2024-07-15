175cf25a98dbba1e30ce9363c9b17b803bf34524
Merna <mernaehab783@gmail.com>
2024-07-15 22:42:38 +0300

    final  commit

--- src/FetchData/FetchData.jsx
+++ src/FetchData/FetchData.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001';
+const API_URL = 'https://mernaehab404.github.io/TransactionServer/db.json';
 
 export const fetchCustomers = () => axios.get(`${API_URL}/customers`);
 export const fetchTransactions = () => axios.get(`${API_URL}/transactions`);
